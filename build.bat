a3fa2074e7820ee5387e25c782883ebf8d645eda
Tejeda, Engelbert <etejeda@seic.com>
2018-08-31 11:43:55 -0400

    First working version of the powershell equivalent of build.sh

--- build.bat
+++ build.bat
@@ -5,8 +5,8 @@
 @@if defined POWERSHELL_BAT_ARGS set POWERSHELL_BAT_ARGS=%POWERSHELL_BAT_ARGS:"=\"%
 @@PowerShell -noprofile -Command Invoke-Expression $('$args=@(^&{$args} %POWERSHELL_BAT_ARGS%);'+[String]::Join(';',$((Get-Content '%~f0') -notmatch '^^@@'))) & pause & goto :EOF
 Set-ExecutionPolicy Bypass -Scope Process -Force
-$PREFIX = "eval"
 $DEFAULT_TEMPLATE = "_template/templates/default.html"
+$DEFAULT_OUTPUT_FILEEXT = "hta"
 $DEFAULT_HEADER = "_common/templates/header.html"
 $DEFAULT_CSS = "_common/templates/default.css"
 $t=1
@@ -20,7 +20,7 @@ $params=@{'--source|-s$' =  "[some/markdown/file.md,some/other/markdown/file2.md
 '--watchdir|-d$' =  "[somedir]";
 '--watch|-w$' =  "[somefile1.md,somefile2.html,*.txt,*.md,*.js,*.etc]";
 '--interval|-i$' =  "[t>0]";
-'--dry' =  "Dry Run";
+'--dry|-y$' =  "Dry Run";
 }
 FUNCTION Usage {
     WRITE-HOST "Usage: build.bat"
@@ -31,19 +31,98 @@ FUNCTION Usage {
 $i=0 
 If (-Not $($ARGS)){
 	Usage
+	Exit
 }
 ForEach ($ARG In $ARGS) {
+	$ArgValue = $($ARGS[$i+1])
 	ForEach ($param In $params.Keys) {
 		If ($ARG -match $param){
-			If (-Not $($ARGS[$i+1])){
-				$expression = "`$$($param.substring(0,$param.lastindexOf('|')).replace('-','')) = 'True'"
+			If (-Not $ArgValue){
+				try{
+					$expression = "`$$($param.substring(0,$param.lastindexOf('|')).replace('-','')) = 'True'"
+				} catch {
+					Write-Host "Encountered an error in processing $param`: If this is a switch, make sure it is of the form '--switch|-s'"
+					Exit
+				}
 			} else {
-				$expression = "`$$($param.substring(0,$param.lastindexOf('|')).replace('-','')) = '$($ARGS[$i+1])'"
+				try{
+					$paramvar = $($param.substring(0,$param.lastindexOf('|')).replace('-',''))
+				} catch {
+					"Encountered an error in processing $param`: Value provided was $ArgValue"
+					Exit
+				}
+				$expression = "If ( `"$paramvar`" -match 'var') { `$$paramvar += '$($ArgValue)_@DELIM@_' } else { `$$paramvar = '$($ArgValue)' }"
 			}
 			Invoke-Expression $expression
 		}
 	}
 	$i++
 }
-If ($help){Usage}
 
+@@:: Show Help If Applicable
+If ($help) { Usage }
+
+If ( get-command pandoc -ErrorAction silentlycontinue ){
+	$pandoc = $(get-command pandoc).Path
+} ElseIf ( Test-Path "$($PWD.PATH)\pandoc.exe" ) {
+	$pandoc = "$($PWD.PATH)\pandoc.exe"
+}
+If (get-command pp -ErrorAction silentlycontinue){
+	$pp = $(get-command pp).Path
+} ElseIf ( Test-Path "$($PWD.PATH)\pp.exe" ) {
+	$pp = "$($PWD.PATH)\pp.exe"
+}
+
+@@:: Check for required binaries
+If ( -Not (Test-Path $pandoc) -or -Not (Test-Path $pp) ) { 
+	"Error: Neither pp or pandoc were not found in your path or in the current working directory"
+	Exit
+}
+
+@@:: Build pre-processor commands
+$pp_commands = "&$($pp) "
+If ( $ppvars ) {
+    $ppvars = $ppvars.replace("_@DELIM@_"," -D ")
+    $pp_commands = "$($pp_commands) -D $($ppvars.Substring(0,$ppvars.Length-3)) "    
+}
+$pp_commands += "$source "
+@@:: Build pandoc commands
+$output_file = If ($output) {$output} Else {"$($source.substring(0,$source.lastindexOf("."))).$($DEFAULT_OUTPUT_FILEEXT)"}
+$pandoc_commands = "$pandoc "
+$pandoc_commands += "-o '$output_file' "
+$css = If ($css) {$css} Else {$DEFAULT_CSS}
+$pandoc_commands += "-c '$css' "
+$header = If ($header) {$header} Else {$DEFAULT_HEADER}
+$pandoc_commands += "-H '$header' "
+$template = If ($template) {$template} Else {$DEFAULT_TEMPLATE}
+$pandoc_commands += "--template $template "
+If ($vars) {
+	If ($vars -match "_@DELIM@_"){
+		$vars = $vars.replace("_@DELIM@_"," -V ")
+		$pandoc_commands += "-V $($vars.Substring(0,$vars.Length-3)) "
+	} else {
+		$pandoc_commands += "-V $vars "
+	}
+}
+If ($metavars) {
+	$metavars = $metavars.replace("_@DELIM@_"," --metadata ")
+	$pandoc_commands += "--metadata $($metavars.Substring(0,$metavars.Length-11)) "
+}
+$pandoc_commands += "--self-contained "
+$pandoc_commands += " --standalone "
+@@:: Echo commands if this is a Dry Run
+If ($dry) {
+	Write-Host "$pp_commands | $pandoc_commands"
+
+} else {
+	try{
+		"Issuing build"
+		Invoke-Expression "$pp_commands | $pandoc_commands"
+		"Done. Output file is $output_file"
+	} catch {
+		"Failed to execute commands`:"
+		"$pp_commands | $pandoc_commands"
+		"Errors`:"
+		$_.Exception
+	}
+}
